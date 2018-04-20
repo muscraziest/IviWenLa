77ccecb5a719ac754ed0aa03e46d0fedc66adc3a
Unknown <ltiradolopz@gmail.com>
2018-04-20 18:17:01 +0200

    Hasta el toto

--- app/src/main/java/edu/wenla/vivihome/MainActivity.java
+++ app/src/main/java/edu/wenla/vivihome/MainActivity.java
@@ -1,9 +1,13 @@
 package edu.wenla.vivihome;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.speech.RecognizerIntent;
@@ -13,6 +17,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,7 +25,9 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends VoiceActivity {
+import static android.view.View.*;
+
+public class MainActivity extends VoiceActivity implements View.OnClickListener {
 
     private static final String LOGTAG = "TALKBACK";
     private static Integer ID_PROMPT_QUERY = 0;
@@ -49,13 +56,39 @@ public class MainActivity extends VoiceActivity {
         luz_lectura = 0;
         luz_regulable = 0;
         abrir_puerta = 0;
+
+        controlador();
+    }
+
+    @TargetApi(VERSION_CODES.M)
+    public void controlador(){
+
+        Button entry = findViewById(R.id.settings_btn);
+
+        entry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent opciones = new Intent(getApplicationContext(),SettingsActivity.class);
+                startActivity(opciones);
+            }
+
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        return true;
+        //if(item.getItemId() == R.id.){
+            return true;
+
+            //Intent about = new Intent(getApplicationContext(),About.class)
+            //startActivity(about);
+            //return true;
+        //}
+
+        //return super.onOptionsItemSelected(item);
     }
+
 
     @Override
     public void onDestroy() {
@@ -65,6 +98,8 @@ public class MainActivity extends VoiceActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
+        //getMenuInflater().inflate(R.menu.options,menu);
         return true;
     }
     /////////////////////////////////////////////////////////////////////////////////////
@@ -84,7 +119,7 @@ public class MainActivity extends VoiceActivity {
     private void setSpeakButton() {
         // gain reference to speak button
         Button speak = (Button) findViewById(R.id.speech_btn);
-        speak.setOnClickListener(new View.OnClickListener() {
+        speak.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Ask the user to speak
@@ -313,5 +348,10 @@ public class MainActivity extends VoiceActivity {
     @Override
     public void onTTSStart(String uttId) {
         Log.e(LOGTAG, "TTS starts speaking");
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
